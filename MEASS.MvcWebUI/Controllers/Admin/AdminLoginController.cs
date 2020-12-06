edc385ec39028fd417228dd7bcc336ac911c95ff
Ömür Gün <omur_gun@outlook.com>
2020-12-06 19:47:06 +0300

    commit

--- MEASS.MvcWebUI/Controllers/Admin/AdminLoginController.cs
+++ MEASS.MvcWebUI/Controllers/Admin/AdminLoginController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MEAAS.Business.Abstract;
 using MEAAS.Business.DependencyResolvers.Ninject;
+using MEASS.MvcWebUI.Models;
 
 namespace MEASS.MvcWebUI.Controllers.Admin
 {
@@ -18,26 +19,27 @@ namespace MEASS.MvcWebUI.Controllers.Admin
             return View();
         }
 
-        public string Login()
+        public ActionResult Login(Models.Admin admin)
         {
-            var adminName = "verytr";
-            var password = "1234";
+            
+            var adminName = admin.AdminName;
+            var password = admin.AdminPassword;
 
             if (adminName == string.Empty || password == string.Empty)
             {
-                return "can't be blank";
+                return RedirectToAction("Error", "Default");
             }
             else
             {
-                var admin = _adminService.GetAdminByAdminNameAndPassword(adminName, password);
-                if (admin != null)
+                var _admin = _adminService.GetAdminByAdminNameAndPassword(adminName, password);
+                if (_admin != null)
                 {
 
-                    return "Login successful..";
+                    return RedirectToAction("Index", "AdminHome");
                 }
                 else
                 {
-                    return "Admin name or password is incorrect!";
+                    return RedirectToAction("Error", "Default");
                 }
             }
         }
---
+++ MEASS.MvcWebUI/Controllers/Admin/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MEASS.MvcWebUI.Controllers.Admin
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
